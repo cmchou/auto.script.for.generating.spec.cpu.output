a0a2c62d5008fa3acb075107c6e590ca7eb5d1e9
Jamie Chou <cmchou@cmchou.net>
2015-05-13 15:27:50 +0800

    Update for auto output pdf

--- auto.sh
+++ auto.sh
@@ -1,4 +1,26 @@
-ssh root@x3650m5 'pwd'
-ssh root@x3650m5 'cd /'
-ssh root@x3650m5 'scp root@10.x.x.x:/.../speccpu'
-ssh root@x3650m5 'mkdir'
+#!/bin/bash 
+Pwd=$(pwd)
+RSFPATH="${PWD}/*.rsf"
+Num_file=0
+for f in $RSFPATH
+do
+  Num_file=$((Num_file+1))
+done
+echo $Num_file
+echo "${PWD}"
+
+# try to source shrc in SPEC directory
+cd ~/SPEC  #switch to spec binary directory
+echo ${Pwd}
+. ./shrc   #source environment
+cd ${Pwd}
+ls
+
+for f in $RSFPATH
+do
+   echo $f
+   cp $f ${f}.sub
+#   rawformat -o txt --flagsurl  ~/Desktop/config/Intel-ic14.0-official-linux64.xml, ~/Desktop/config/Lenovo-Platform-Flags-V1.2-HSW-A.xml ${f}.sub
+rawformat -o pdf ${f}.sub
+done
+
